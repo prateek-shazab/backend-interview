698ab75555a1d7090aeb605e6a36fb6e11977351
anmol-shazab <anmol@shazab.in>
2024-03-31 13:10:56 +0530

    Minor. Update variable name.

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ const logger = require('./helpers/_logger');
 const { API_BASE, PORT } = require('./scripts/constants');
 
 // ** Routes
-const userRoutes = require('./routes/UserRoutes');
+const UserRoutes = require('./routes/UserRoutes');
 
 const webApp = express();
 
-webApp.use(API_BASE + '/user', userRoutes);
+webApp.use(API_BASE + '/user', UserRoutes);
 
 webApp.listen(PORT, () => {
   logger.info('App is listening at port', PORT);
