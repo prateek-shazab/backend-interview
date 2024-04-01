c77dc627e74d4f3e14d656ccdbda2bc9a15741d8
anmol-shazab <anmol@shazab.in>
2024-04-01 18:36:40 +0530

    Update UserService.

--- services/UserService.js
+++ services/UserService.js
@@ -1,5 +1,11 @@
-const logger = require('../helpers/logger');
+// ** Models
 const { UserModel } = require('../models');
+
+// ** Helpers
+const logger = require('../helpers/logger');
+
+const USER_LIST_LIMIT = 20;
+const USER_LIST_POP_DEF = [{ path: 'businessId' }];
 
 class UserService {
   constructor() {
@@ -7,7 +13,9 @@ class UserService {
   }
 
   async getUserList() {
-    return UserModel.find({});
+    return UserModel.find({})
+      .populate(USER_LIST_POP_DEF)
+      .limit(USER_LIST_LIMIT);
   }
 }
 
